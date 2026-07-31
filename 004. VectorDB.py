62dc2c977efc072d93dcc26b59d3845a416c0c31
duembi <161377352+duembi@users.noreply.github.com>
2026-07-31 09:07:19 +0300

    Add persistent ChromaDB client and collection setup
    Implement a persistent ChromaDB client and collection creation with embedding functionality.

---
+++ 004. VectorDB.py
@@ -0,0 +1,56 @@
+
+#chromaDB yi iki farklı şekilde çalıştırabilirz, biz kalıcı olan vektör DB kullanıcaz.
+#Koleksiyonumuzu tanımlayıp, onu embdeding modeline vereceğiz
+#otomatik embedding kurulacak
+#metadata hazırlanacak, vereceğimi zverinin hangi kaynaktan geldiği bilgisni verir bize
+
+
+import chromadb
+from chromadb.utils import embedding_functions
+
+#vektör veritabnı için dosya yolu 
+vector_database_path = "./"
+
+#sentenceTransformer modelini kullanarak bir embedding fonksiyonu oluştur
+#bu fonksiyon, metin parlaçarlarını vektöre dönüştürmek için kullanaılacak
+
+#embedding modeli seçmi, sorgular ve metin parçaları bu modelden geçip vektörlere çevirilecek. Max 128 token!!
+sentence_transformer_model = "distiluse-base-multilingual-cased-v1"
+
+#fonskiyon oluşturma
+embedding_function = embedding_functions.SentenceTransformerEmbeddingFunction(model_name = sentence_transformer_model)
+
+#persistent bir chromaDB istemcisi/clientı oluşturacağız
+#bu sayede vektör veritabanının diske kaydedilmesini ve oturumlar arasında kalıcı olmasını sağlar
+def create_chroma_client(vector_database_path, collection_name, embedding_function):
+    chroma_client = chromadb.PersistentClient(path = vector_database_path)
+
+    #burada bir collection oluşturuyoruz ama bu isiimde başka bir collection olabilir onun kontrolünü yapıyorum
+    try :
+        chroma_collection = chroma_client.get_collection(collection_name)
+
+        #eğer bu isimde bir koleksiyon mevcutsa sil
+        print(f"'{collection_name}' name existing. Deleting..")
+        chroma_client.delete_collection(collection_name)
+        print(f"'{collection_name}' deleted..")
+
+        #eğer sildiysen silme işleminden sonra collection ı yeniden oluştur
+        chroma_collection = chroma_client.create_collection(collection_name, embedding_function = embedding_function)
+        print(f"'collection_name' collection created again..")
+
+    except :
+        #eğer koleksiyon mevcut değilse oluştur
+        chroma_collection = chroma_client.create_collection(collection_name, embedding_function = embedding_function)
+        print(f"'{collection_name}' created..")
+
+#oluşturulan client ve collection ı döndür
+    return chroma_client, chroma_collection    
+
+collection_name = "rag_chunks"
+chroma_client, chroma_collection = create_chroma_client(vector_database_path, collection_name, embedding_function)
+
+print("Collection created !!")
+
+print(f"\tCollection is in ChromaDB : {chroma_client.list_collections()}")
+print(f"Total file number : {chroma_collection.count()}")
+
